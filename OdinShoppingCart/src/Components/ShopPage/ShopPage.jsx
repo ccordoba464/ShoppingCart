2a5f34b0c84344dc0e3cca290fdc4b828cad3b3d
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-09-02 16:08:13 -0400

    Populate shop page with info and various products

--- OdinShoppingCart/src/Components/ShopPage/ShopPage.jsx
+++ OdinShoppingCart/src/Components/ShopPage/ShopPage.jsx
@@ -1,15 +1,49 @@
 import styles from "./ShopPage.module.css";
+import Bloom from "../../Images/BLOOMORBOOM.png";
+import Calculated from "../../Images/CALCULATED.jpeg";
+import HOW from "../../Images/HOW.jpg";
+import VV1 from "../../Images/VV1.jpg";
+import midEverything from "../../Images/midEVERYTHING.jpg";
+import TIR from "../../Images/TrustInRimbo.jpg";
 
 function ShopPage() {
   return (
     <>
-      <div className={styles.productContainer}>
-        <div className={styles.productCard}></div>
-        <div className={styles.productCard}></div>
-        <div className={styles.productCard}></div>
-        <div className={styles.productCard}></div>
-        <div className={styles.productCard}></div>
-        <div className={styles.productCard}></div>
+      <div className={styles.shopContainer}>
+        <div className={styles.title}>Vowel Gang Shop</div>
+        <div className={styles.productContainer}>
+          <div className={styles.productCard}>
+            <img src={Bloom} alt="Bloom or Boom" className={styles.itemImage} />
+          </div>
+          <div className={styles.productCard}>
+            <img
+              src={Calculated}
+              alt="Calculated Cover"
+              className={styles.itemImage}
+            />
+          </div>
+          <div className={styles.productCard}>
+            <img src={HOW} alt="HOW Cover" className={styles.itemImage} />
+          </div>
+          <div className={styles.productCard}>
+            <img src={VV1} alt="VV1 Cover" className={styles.itemImage} />
+          </div>
+          <div className={styles.productCard}>
+            <img
+              src={midEverything}
+              alt="midEverything Cover"
+              className={styles.itemImage}
+            />
+          </div>
+          <div className={styles.productCard}>
+            {" "}
+            <img
+              src={TIR}
+              alt="Trust In Rimbo Cover"
+              className={styles.itemImage}
+            />
+          </div>
+        </div>
       </div>
     </>
   );
