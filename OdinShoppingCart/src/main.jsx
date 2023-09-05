de33b810c77af1b778f4897fe0433fd78e46a968
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-09-05 19:52:15 -0400

    test

--- OdinShoppingCart/src/main.jsx
+++ OdinShoppingCart/src/main.jsx
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./Components/App/App.jsx";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./Components/HomePage/HomePage.jsx";
-import ShopPage from "./Components/ShopPage/ShopPage.jsx";
-import CartPage from "./Components/CartPage/CartPage.jsx";
 
 const router = createBrowserRouter([
   {
